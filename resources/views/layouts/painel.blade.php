7e06fef8f509c4ede8eb7329ec2fa39d092f5834
Kleber Faustino <faustikle@gmail.com>
2017-08-27 14:33:22 -0300

    Correcao do link para painel na index do painel

--- resources/views/layouts/painel.blade.php
+++ resources/views/layouts/painel.blade.php
@@ -18,7 +18,7 @@
         <div class="col-md-3 left_col">
             <div class="left_col scroll-view">
                 <div class="navbar nav_title" style="border: 0;">
-                    <a href="#" class="site_title"><i class="fa fa-building"></i> <span>Painel GCON</span></a>
+                    <a href="{{ route('painel.index') }}" class="site_title"><i class="fa fa-building"></i> <span>Painel GCON</span></a>
                 </div>
 
                 <div class="clearfix"></div>
