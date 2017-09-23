65a71f6b6f35034a25e74f18e9fbaf3be30021a8
Kleber Faustino <faustikle@gmail.com>
2017-09-23 18:48:15 -0300

    Criado middleware para setar locale pt-br no Carbon

--- app/Http/Middleware/ApplyCarbonLocale.php
+++ app/Http/Middleware/ApplyCarbonLocale.php
@@ -2,7 +2,20 @@
 
 namespace App\Http\Middleware;
 
+use Carbon\Carbon;
+use Closure;
+
 final class ApplyCarbonLocale
 {
+    /**
+     * @param $request
+     * @param Closure $next
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function handle($request, Closure $next)
+    {
+        Carbon::setLocale(config('app.locale'));
 
+        return $next($request);
+    }
 }
